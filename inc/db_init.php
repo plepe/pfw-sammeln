a594c17648b60f8738f9e8d07fa87b16cbc8ea4f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-05-19 09:07:35 +0200

    Improve database connection: utf8, use assoc fetch as result

--- inc/db_init.php
+++ inc/db_init.php
@@ -9,7 +9,9 @@ function db_init () {
   }
 
   try {
-    $db = new PDOext($db_conf, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $db_conf[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
+    $db = new PDOext($db_conf);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   }
   catch (Exception $e) {
     print "Can't connect to database: " . $e->getMessage();
