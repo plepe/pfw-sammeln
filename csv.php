36ec3f05fb83829e85b5f515a705a4b78d611486
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-05-23 11:10:54 +0200

    CSV: when requested, export only one entry

--- csv.php
+++ csv.php
@@ -5,7 +5,12 @@
 <?php // App functionality
 db_init();
 
-$req = $db->query("select id, sammlerin, datum, eintrag_datum, (plz1010 + plz1020 + plz1030 + plz1040 + plz1050 + plz1060 + plz1070 + plz1080 + plz1090 + plz1100 + plz1110 + plz1120 + plz1130 + plz1140 + plz1150 + plz1160 + plz1170 + plz1180 + plz1190 + plz1200 + plz1210 + plz1220 + plz1230) as sum, plz1010, plz1020, plz1030, plz1040, plz1050, plz1060, plz1070, plz1080, plz1090, plz1100, plz1110, plz1120, plz1130, plz1140, plz1150, plz1160, plz1170, plz1180, plz1190, plz1200, plz1210, plz1220, plz1230, anmerkungen from unterschriften_listen order by eintrag_datum asc");
+if (array_key_exists('id', $_REQUEST)) {
+  $req = $db->query("select id, sammlerin, datum, eintrag_datum, (plz1010 + plz1020 + plz1030 + plz1040 + plz1050 + plz1060 + plz1070 + plz1080 + plz1090 + plz1100 + plz1110 + plz1120 + plz1130 + plz1140 + plz1150 + plz1160 + plz1170 + plz1180 + plz1190 + plz1200 + plz1210 + plz1220 + plz1230) as sum, plz1010, plz1020, plz1030, plz1040, plz1050, plz1060, plz1070, plz1080, plz1090, plz1100, plz1110, plz1120, plz1130, plz1140, plz1150, plz1160, plz1170, plz1180, plz1190, plz1200, plz1210, plz1220, plz1230, anmerkungen from unterschriften_listen where id=" . $db->quote($_REQUEST['id']));
+}
+else {
+  $req = $db->query("select id, sammlerin, datum, eintrag_datum, (plz1010 + plz1020 + plz1030 + plz1040 + plz1050 + plz1060 + plz1070 + plz1080 + plz1090 + plz1100 + plz1110 + plz1120 + plz1130 + plz1140 + plz1150 + plz1160 + plz1170 + plz1180 + plz1190 + plz1200 + plz1210 + plz1220 + plz1230) as sum, plz1010, plz1020, plz1030, plz1040, plz1050, plz1060, plz1070, plz1080, plz1090, plz1100, plz1110, plz1120, plz1130, plz1140, plz1150, plz1160, plz1170, plz1180, plz1190, plz1200, plz1210, plz1220, plz1230, anmerkungen from unterschriften_listen order by eintrag_datum asc");
+}
 
 if (!$req) {
   print("Fehler beim Eintragen in die Datenbank: " . $db->errorInfo()[2]);
