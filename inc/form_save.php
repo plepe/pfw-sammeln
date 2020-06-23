9ff68d9a26b43bd8eedb9f9660f3649305fdc137
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2020-06-23 09:44:46 +0200

    form_save: improve error handling; check if data exists after entering

--- inc/form_save.php
+++ inc/form_save.php
@@ -17,7 +17,14 @@ function form_save ($form_data) {
   $set = implode(', ', $set);
   $result = $db->query("insert into unterschriften_listen set {$set}");
   if (!$result) {
-    messages_debug("Fehler beim Eintragen in die Datenbank: " . $db->errorInfo()[2]);
+    messages_add("Fehler beim Eintragen in die Datenbank: " . $db->errorInfo()[2], MSG_ERROR);
+    return;
+  }
+
+  $result = $db->query('select * from unterschriften_listen where id=' . $db->quote($id));
+  if ($result->rowCount() !== 1) {
+    messages_add("Fehler beim Eintragen in die Datenbank: Kann den Eintrag nicht finden!", MSG_ERROR);
+    return;
   }
 
   file_put_contents("data/{$id}.json", json_encode($data));
